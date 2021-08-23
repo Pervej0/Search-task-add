2b3298b42cf962970c217f0a75644ed28f85d214
Pervej0 <mdparvez222khan@gmail.com>
2021-08-23 16:42:46 +0600

    search functionality added.

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
+let taskField = document.getElementById("task-field");
+
+function searchTask(id) {
+  const tasks = document.querySelectorAll(".task-box");
+  const search = document.getElementById("searchBox");
+
+  document.getElementById(id).addEventListener("click", function (event) {
+    event.preventDefault();
+    let searchedTask = [];
+    for (task of tasks) {
+      let userTaskText = task.children[0].innerText;
+      if (
+        userTaskText.toLowerCase().includes(search.value.toLowerCase()) == true
+      ) {
+        task.style.display = "block";
+      } else {
+        task.style.display = "none";
+      }
+    }
+
+    // taskField.appendChild(...searchedTask);
+    // console.log(searchedTask);
+    //  parent.inerHTML = (...searchedTask);
+  });
+}
+
+// task button click functionality===
+function taskButton(taskParent) {
+  let buttonElements = document.getElementsByClassName("task-button");
+  let unDone = document.getElementById("unDone");
+  unDone.innerText = taskParent.childElementCount;
+  for (buttonElement of buttonElements) {
+    // function()
+    buttonElement.addEventListener("click", function (event) {
+      if (event.target.innerText == "Done") {
+        event.target.setAttribute("disabled", true);
+        event.target.previousSibling.style.textDecoration = "line-through";
+        event.target.innerText = "Compleated";
+        let done = document.getElementById("done");
+        done.innerText++;
+        unDone.innerText =
+          parseInt(totalTask.innerText) - parseInt(done.innerText);
+      }
+    });
+  }
+}
+
+// createing task --
 function createTask() {
-  let taskField = document.getElementById("task-field");
   let inputText = document.getElementById("inputField");
 
   // createing task component--
@@ -8,7 +55,7 @@ function createTask() {
   const button = document.createElement("button");
 
   if (inputText.value == "" || inputText.value.indexOf("!") != -1) {
-    console.log("Please add valid text in your input!");
+    return console.log("Please add valid text in your input!");
   }
   div.classList.add(
     "task-item",
@@ -21,41 +68,21 @@ function createTask() {
   p.innerText = inputText.value;
   p.style.color = "#000000";
   p.style.fontSize = "20px";
+  p.style.textTransform = "capitalize";
   button.classList.add("btn", "btn-success", "task-button");
   button.innerText = "Done";
   div.appendChild(p);
   div.appendChild(button);
+  div.classList.add("task-box");
   taskField.appendChild(div);
   inputText.value = "";
 
   let totalTask = document.getElementById("totalTask");
   totalTask.innerText = taskField.childElementCount;
-
-  // task button click functionality===
-
-  function taskButton() {
-    let buttonElements = document.getElementsByClassName("task-button");
-    for (buttonElement of buttonElements) {
-      // function()
-      buttonElement.addEventListener("click", function (event) {
-        // debugger;
-        if (event.target.innerText == "Done") {
-          event.target.setAttribute("disabled", true);
-          event.target.previousSibling.style.textDecoration = "line-through";
-          event.target.innerText = "Compleated";
-          let done = document.getElementById("done");
-          done.innerText++;
-          console.log(done.innerText);
-          let unDone = document.getElementById("unDone");
-          unDone.innerText =
-            parseInt(totalTask.innerText) - parseInt(done.innerText);
-        }
-      });
-    }
-  }
-  taskButton();
+  taskButton(taskField);
+  searchTask("searchBtn");
 }
 
-addTask.addEventListener("click", function (event) {
-  createTask(event.target.parentElement.parentElement.nextElementSibling);
+addTask.addEventListener("click", function () {
+  createTask();
 });
